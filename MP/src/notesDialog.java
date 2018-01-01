9f3603720d86cedb1b296c3e7d96b2b570869b40
Khen <khendror@Khens-MacBook-Pro.local>
2018-01-01 20:05:51 +0200

    dialog

--- MP/src/notesDialog.java
+++ MP/src/notesDialog.java
@@ -5,12 +5,15 @@ import java.awt.event.*;
 public class notesDialog extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
-    private JButton buttonCancel;
+    private JButton buttonDelete;
     private JComboBox durationBox;
     private JButton[] buttons;
     private session a;
     private JLabel label1;
     private JLabel label2;
+    private String chord;
+    private String duration;
+    private JButton selectedButton;
 
 
     public notesDialog(session a) {
@@ -19,7 +22,7 @@ public class notesDialog extends JDialog {
 
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
-        buttonOK.setBounds(300,200,20,20);
+
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -27,11 +30,19 @@ public class notesDialog extends JDialog {
             }
         });
 
-        buttonCancel.addActionListener(new ActionListener() {
+        buttonDelete.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onCancel();
+                onDelete();
             }
         });
+
+        buttonDelete.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onDelete();
+            }
+        });
+
+
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
@@ -52,22 +63,41 @@ public class notesDialog extends JDialog {
         pane.setLayout(null);
         createButtons(pane);
 
+
     }
 
     private void createButtons(Container pane) {
+        ActionListener buttonAction = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(chord != null) {
+                    selectedButton.setEnabled(true);
+                    selectedButton.setBackground(null);
+                }
+                JButton b = (JButton) e.getSource();
+                b.setOpaque(true);
+                b.setEnabled(false);
+                b.setBackground(Color.green);
+                chord = b.getText();
+                selectedButton = b;
+            }
+        };
+
         String[] notes = this.a.getKeyNotes();
         buttons = new JButton[notes.length+1];
         int x = 0;
         for (int i = 0; i < notes.length; i++){
             buttons[i] = new JButton(notes[i]);
             buttons[i].setBounds(10 + x,30,40,40);
+            buttons[i].addActionListener(buttonAction);
             pane.add(buttons[i]);
             x += 40;
         }
         buttons[buttons.length-1] = new JButton("Break");
         pane.add(buttons[buttons.length-1]);
         this.buttonOK.setBounds(230,140,70,30);
-        this.buttonCancel.setBounds(0,140,70,30);
+        this.buttonDelete.setBounds(0,140,70,30);
+        this.buttonDelete.setText("Delete");
         String[] list = { "1", "1/2", "1/8", "1/16", "1/32" };
         this.durationBox = new JComboBox(list);
         this.durationBox.setBounds(100, 70,100,100);
@@ -79,14 +109,21 @@ public class notesDialog extends JDialog {
         pane.add(this.label1);
         pane.add(this.label2);
         pane.add(this.buttonOK);
-        pane.add(this.buttonCancel);
+        pane.add(this.buttonDelete);
         pane.add(this.durationBox);
 
 
     }
 
     private void onOK() {
-        // add your code here
+        this.duration = this.durationBox.getSelectedItem().toString();
+        if (this.chord != null) {
+            dispose();
+        }
+    }
+
+    private void onDelete() {
+        // add your code here if necessary
         dispose();
     }
 
@@ -98,7 +135,7 @@ public class notesDialog extends JDialog {
     public static void main(String[] args) {
         session a = new session();
         notesDialog dialog = new notesDialog(a);
-        dialog.setSize(300, 200); // << not working!!!
+        dialog.setSize(300, 200);
         dialog.setVisible(true);
         System.exit(0);
     }
