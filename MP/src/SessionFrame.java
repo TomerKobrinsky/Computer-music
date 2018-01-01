30a90311c86c5ba5e6af5d441e8174c667b067ef
Roei5591 <34808057+Roei5591@users.noreply.github.com>
2018-01-01 18:35:00 +0200

    hi

---
+++ MP/src/SessionFrame.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SessionFrame extends JFrame {
+
+    private JPanel panel1;
+    private JButton buttons[];
+    private JLabel labels[];
+
+    public SessionFrame()
+    {
+        super();
+        panel1 = new JPanel() ;
+        panel1.setLayout(null);
+
+        panel1.setSize(750 , 500);
+        this.setContentPane(panel1);
+        this.setSize(750, 500);
+
+
+        buttons = new JButton[30];
+        labels = new JLabel[30];
+
+
+        ActionListener t  = (new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                JButton b = (JButton)e.getSource();
+                System.out.println(b.getName());
+            }
+        });
+
+        for(int i = 0; i < 30; i++)
+        {
+            labels[i] = new JLabel();
+            labels[i].setText("" + i);
+            buttons[i] = new JButton("");
+            //buttons[i].setFont(new Font("Ariel", Font.PLAIN, 10));
+            buttons[i].setName("" + i);
+            //buttons[i].setLayout(null);
+            buttons[i].setBounds(10 + i*20, 200, 20 ,60);
+            labels[i].setBounds(12 + i*20, 180, 20 ,20);
+            buttons[i].addActionListener(t);
+
+            panel1.add(labels[i]);
+            panel1.add(buttons[i]);
+
+
+        }
+    }
+
+}
